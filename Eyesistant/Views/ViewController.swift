4a234f9288e91968e0a9bf62b029921d303d0178
vinchen <vinchen996@gmail.com>
2024-08-21 17:28:50 +0700

    Added skintone detection page, added recommended colors

--- Eyesistant/Views/ViewController.swift
+++ Eyesistant/Views/ViewController.swift
@@ -9,11 +9,25 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet var NavigateButton:UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
-
-
+    
+    @IBAction func navigateToNewPage(_ sender:UIButton){
+        performSegue(withIdentifier: "goToNewPage", sender: self)
+    }
+    
+//    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+//        if segue.identifier == "goToNewPage"{
+//            // Create a variable that you want to send
+//            var newProgramVar = 1
+//            
+//            // Create a new variable to store the instance of PlayerTableViewController
+//            let destinationVC = segue.destination as! SkinToneDetectorViewController
+//            destinationVC.a = newProgramVar
+//        }
+//    }
 }
-
